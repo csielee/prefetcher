87bd5b47b423b3eeb966217aea06abb3b80902ef
csielee <lee097019@gmail.com>
2017-03-16 00:41:39 +0800

    Let $ make can generate different exe file
    generate main,main_sse,main_sse_prefetch,main_native

--- Makefile
+++ Makefile
@@ -1,13 +1,25 @@
 CFLAGS = -msse2 --std gnu99 -O0 -Wall -Wextra
 
+SSE = -DSSE=1
+
+SSE_PREFETCH = -DSSE_PREFETCH=1
+
+NATIVE = -DNATIVE=1
+
 GIT_HOOKS := .git/hooks/applied
 
 all: $(GIT_HOOKS) main.c
-	$(CC) $(CFLAGS) -o main main.c
+	$(CC) $(CFLAGS) $(SSE) $(SSE_PREFETCH) $(NATIVE) -o main main.c
+	$(CC) $(CFLAGS) $(SSE) -o main_sse main.c
+	$(CC) $(CFLAGS) $(SSE_PREFETCH) -o main_sse_prefetch main.c
+	$(CC) $(CFLAGS) $(NATIVE) -o main_native main.c
 
 $(GIT_HOOKS):
 	@scripts/install-git-hooks
 	@echo
+
+astyle:
+	astyle --style=kr --indent=spaces=4 --indent-switches --suffix=none *.[ch]
 
 clean:
 	$(RM) main
--- main.c
+++ main.c
@@ -60,34 +60,35 @@ int main()
     {
         struct timespec start, end;
         int *src  = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out0 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out1 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out2 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+        int *out  = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
 
         srand(time(NULL));
         for (int y = 0; y < TEST_H; y++)
             for (int x = 0; x < TEST_W; x++)
                 *(src + y * TEST_W + x) = rand();
-
+#ifdef SSE_PREFETCH
         clock_gettime(CLOCK_REALTIME, &start);
-        sse_prefetch_transpose(src, out0, TEST_W, TEST_H);
+        sse_prefetch_transpose(src, out, TEST_W, TEST_H);
         clock_gettime(CLOCK_REALTIME, &end);
         printf("sse prefetch: \t %ld us\n", diff_in_us(start, end));
+#endif
 
+#ifdef SSE
         clock_gettime(CLOCK_REALTIME, &start);
-        sse_transpose(src, out1, TEST_W, TEST_H);
+        sse_transpose(src, out, TEST_W, TEST_H);
         clock_gettime(CLOCK_REALTIME, &end);
         printf("sse: \t\t %ld us\n", diff_in_us(start, end));
+#endif
 
+#ifdef NATIVE
         clock_gettime(CLOCK_REALTIME, &start);
-        naive_transpose(src, out2, TEST_W, TEST_H);
+        naive_transpose(src, out, TEST_W, TEST_H);
         clock_gettime(CLOCK_REALTIME, &end);
         printf("naive: \t\t %ld us\n", diff_in_us(start, end));
+#endif
 
         free(src);
-        free(out0);
-        free(out1);
-        free(out2);
+        free(out);
     }
 
     return 0;
